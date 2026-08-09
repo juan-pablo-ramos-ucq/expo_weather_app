4817b1e8fba1f430909b88c95141af22b5f71fb7
Juan Pablo Ramos Escalona <juan.ramos.dev@gmail.com>
2026-08-09 11:32:36 -0600

    feat: replace GoogleLogin component with Home in Index and add GoogleLoginContainer

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import GoogleLogin from '../components/GoogleLogin';
+import Home from '../components/home';
 
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
-      <GoogleLogin onContinueWithGoogle={() => {}} />
+      <Home />
     </SafeAreaView>
   );
 }
---
+++ components/GoogleLoginContainer.tsx
@@ -0,0 +1,13 @@
+import GoogleLogin from './GoogleLogin';
+
+export default function GoogleLoginContainer() {
+    const handleContinueWithGoogle = async () => {
+        try {
+
+        } catch {
+            
+        }
+    }
+
+    return <GoogleLogin />
+}
